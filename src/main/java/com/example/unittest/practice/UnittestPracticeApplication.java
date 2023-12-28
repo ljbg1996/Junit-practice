5838b6e6aee2c154529c050dce3c2594f04b81e3
ljbg1996 <ljbg1996wang@gmail.com>
2023-12-28 19:51:37 +0100

    init

---
+++ src/main/java/com/example/unittest/practice/UnittestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittest.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unittest/practice/UnittestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittest.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
