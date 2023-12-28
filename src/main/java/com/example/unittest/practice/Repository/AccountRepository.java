5f6e299834b816dfae67f7096ae088171c4795bd
ljbg1996 <ljbg1996wang@gmail.com>
2023-12-28 21:23:09 +0100

    security change

--- src/main/java/com/example/unittest/practice/Repository/AccountRepository.java
+++ src/main/java/com/example/unittest/practice/Repository/AccountRepository.java
@@ -4,5 +4,5 @@ import com.example.unittest.practice.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    // 可以添加自定义的查询方法
+
 }
